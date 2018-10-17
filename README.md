b5af120d69518b945992d46c48e47b2115c29149
Mike Ovchinnikov <ovc.mike@gmail.com>
2018-10-17 15:07:17 +0100

    [master]: Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# web-developer-2
+# Это практические материалы ко второму уроку
+
+1. Файл script.js — содержит основной Javascript код страницы 
+2. Файл style.css — содержит CSS-оформление страницы 
+3. Файл tasks2.txt — содержит домашнее задание
+
+### Справочные материалы:
+
+**Javascript**
+
+Документация и примеры по JS на русском языке
+https://learn.javascript.ru/
+
+**jQuery**
+
+Документация на русском: http://jquerybook.ru/api/
+На английском: https://api.jquery.com/
+
+**HTML/CSS**
+
+Справочник по всем HTML-тегам
+http://htmlbook.ru/html
+
+Справочник по всем CSS-правилам
+http://htmlbook.ru/css
+
+
+**Bootstrap**
+
+Документация по Boostrap на русском языке: http://bootstrap-3.ru/components.php
+
+На английском http://getbootstrap.com/docs/3.3/components/
+
+ 
+ 
---
+++ script.js
@@ -0,0 +1,42 @@
+/** Эта функция будет вызываться на страрте страницы */
+$(function () {
+
+    /** Назначаем обработчик "клика" на кнопку "лайк" **/
+    $(".like-button").click(function (e) {
+
+        //Находим счетчик лайков
+        let likes_counter = $(this).find('.likes');
+
+        //Получаем текущее значение
+        let current_value = parseInt(likes_counter.text());
+
+        //Если новость уже лайкнута
+        if ($(this).hasClass('liked')) {
+            // Удаляем лайк
+            likes_counter.text(current_value - 1);
+            // Меняем стиль кнопки
+            $(this).addClass('btn-default').removeClass('btn-success');
+        } else {
+            // Если новость еще не лайкунта, то добавляем лайк
+            likes_counter.text(current_value + 1);
+            $(this).addClass('btn-success').removeClass('btn-default');
+        }
+
+        //Переключаем класс "liked"
+        $(this).toggleClass('liked');
+
+        //ToDo: Отправлять данные на серевер
+
+        //Запускаем анимации
+        $(this).css('animation', 'like-scale 1s ease-in-out');
+        $(this).find('.glyphicon-thumbs-up').css('animation', 'icon-scale 1s ease-in-out');
+
+        e.preventDefault();
+    });
+
+    /** Назначаем обработчик на кнопку "подробнее" **/
+    $(".show-large-news").click(function () {
+        //ToDO: Загрузить новость с сервера
+        $(".large-news").modal('show');
+    });
+});
